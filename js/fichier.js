854446a1c224cb4f741a4833c3cc009b2a308e17
Adrien BONFILS <Fieldrain@users.noreply.github.com>
2018-01-27 23:40:49 +0100

    use max res thumbnail

--- js/fichier.js
+++ js/fichier.js
@@ -113,7 +113,7 @@ Fichier.prototype.printVideo = function(date,pov){
         let video_thumbnail;
         
         if (youtube_video_id.length == 11) {
-            video_thumbnail = $('<img class="d-block img-fluid" src="//img.youtube.com/vi/'+youtube_video_id+'/0.jpg">');
+            video_thumbnail = $('<img class="d-block img-fluid" src="//img.youtube.com/vi/'+youtube_video_id+'/maxresdefault.jpg">');
         }else{
             video_thumbnail = $('<img class="d-block img-fluid" src="..." alt="No thumbnail">');
         }
