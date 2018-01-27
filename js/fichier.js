3707b88d02a91de4052790e9cd663ceed4baa29d
Adrien BONFILS <Fieldrain@users.noreply.github.com>
2018-01-27 16:59:44 +0100

    test

--- js/fichier.js
+++ js/fichier.js
@@ -23,49 +23,15 @@ Fichier.prototype.getFichier = function(){
         range: 'Feuille 1!A2:E',
     });
 
-    requete.execute(function(reponse){
-        if(!reponse.error){
-            let range = reponse.result;
-            console.log(this);
-            let mapDate = new Map();
-    
-            for(i=0;i<range.values.length;i++){
-                let row = range.values[i];
-                let date = row[0];
-                let map = row[1];
-                let pov = row[2];
-                let lien = row[3];
-    
-                let mapPov;
-                if(mapDate.has(date)){
-                    mapPov = mapDate.get(date);
-                }else{
-                    mapPov = new Map();
-                    mapDate.set(date,mapPov);
-                }
-    
-                let povArray;
-                if(mapPov.has(pov)){
-                    povArray = mapPov.get(pov);
-                }else{
-                    povArray = [];
-                    mapPov.set(pov,povArray);
-                }
-    
-                povArray.push({"Carte" : map,"Url" : lien});
-            }
-        }else{
-            showErrorMessage('Error: ' + reponse.result.error.message);
-        }
-    
-    });
+    requete.execute(this.reponseTableur.bind(this));
 };
 
 Fichier.prototype.reponseTableur = function(reponse){
     if(!reponse.error){
         let range = reponse.result;
-
+        console.log(this);
         let mapDate = new Map();
+        console.log(range);
 
         for(i=0;i<range.values.length;i++){
             let row = range.values[i];
