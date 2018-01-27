55bbebd212ff68de971b624d9d35ead7bb2d71a9
Adrien BONFILS <Fieldrain@users.noreply.github.com>
2018-01-27 16:55:31 +0100

    test

--- js/fichier.js
+++ js/fichier.js
@@ -23,7 +23,42 @@ Fichier.prototype.getFichier = function(){
         range: 'Feuille 1!A2:E',
     });
 
-    requete.execute(this.reponseTableur);
+    requete.execute(function(reponse){
+        if(!reponse.error){
+            let range = reponse.result;
+            console.log(this);
+            let mapDate = new Map();
+    
+            for(i=0;i<range.values.length;i++){
+                let row = range.values[i];
+                let date = row[0];
+                let map = row[1];
+                let pov = row[2];
+                let lien = row[3];
+    
+                let mapPov;
+                if(mapDate.has(date)){
+                    mapPov = mapDate.get(date);
+                }else{
+                    mapPov = new Map();
+                    mapDate.set(date,mapPov);
+                }
+    
+                let povArray;
+                if(mapPov.has(pov)){
+                    povArray = mapPov.get(pov);
+                }else{
+                    povArray = [];
+                    mapPov.set(pov,povArray);
+                }
+    
+                povArray.push({"Carte" : map,"Url" : lien});
+            }
+        }else{
+            showErrorMessage('Error: ' + reponse.result.error.message);
+        }
+    
+    });
 };
 
 Fichier.prototype.reponseTableur = function(reponse){
@@ -31,7 +66,6 @@ Fichier.prototype.reponseTableur = function(reponse){
         let range = reponse.result;
 
         let mapDate = new Map();
-        console.log(range);
 
         for(i=0;i<range.values.length;i++){
             let row = range.values[i];
@@ -58,8 +92,6 @@ Fichier.prototype.reponseTableur = function(reponse){
 
             povArray.push({"Carte" : map,"Url" : lien});
         }
-
-        console.log(mapDate);
     }else{
         showErrorMessage('Error: ' + reponse.result.error.message);
     }
