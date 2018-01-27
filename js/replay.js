6ce7e824372fd8148fe318148296a5a40047f013
Adrien BONFILS <Fieldrain@users.noreply.github.com>
2018-01-27 17:49:12 +0100

    add css +js treeview

--- js/replay.js
+++ js/replay.js
@@ -35,72 +35,60 @@ function Deconnexion(event) {
   delete this.dossier;
 }
 
-/**
- * Recuperation des fichiers avec lien replay
- */
-/*function getFiles() {
-  let request = gapi.client.drive.files.list({
-    q : "name = 'Replay' and mimeType = 'application/vnd.google-apps.folder' and sharedWithMe = true"
-  });
-
-  request.execute(reponseDossier);
-}
-
-function reponseDossier(resp){
-  if (!resp.error) {
-      FOLDER_REPLAY = resp.files[0];
-      lireFichiers();
-  }else{
-      showErrorMessage("Erreur: " + resp.error.message);
-  }
- }
-
- function lireFichiers(){
-  let requestfile = gapi.client.drive.files.list({
-    q : "mimeType = 'application/vnd.google-apps.spreadsheet' and '"+ FOLDER_REPLAY.id+"' in parents"
-  });
-
-  requestfile.execute(reponseFichier);
- }
-
- function reponseFichier(resp){
-  if (!resp.error) {
-      for(i=0;i<resp.files.length;i++){
-        FILES_REPLAY.push(resp.files[i]);
-      } 
-      lireFichier();
-  }else{
-      showErrorMessage("Erreur: " + resp.error.message);
-  }
- }
-
- function lireFichier(){
-  for(let index = 0;index < FILES_REPLAY.length;index++){
-    let fichier = FILES_REPLAY[index];
-    /*let tabFichier = map.get(fichier.name);
-    if(tabFichier == undefined){
-      tabFichier = [];
-      map.set(fichier.name);
-    }
-
-    tabFichier.push(lireFichier(fichier));
-    gapi.client.sheets.spreadsheets.values.batchGet({
-      spreadsheetId: fichier.id,
-      ranges: 'Feuille 1!A:E',
-    }).then(reponseTableur,erreurTableur);
-  }
- }
-
- function reponseTableur(response){
-  let range = response.result;
-  console.log(range);
-      /*if (range.values.length > 0) {
-       
-      } else {
-        
+var defaultData = [
+  {
+    text: 'Parent 1',
+    href: '#parent1',
+    tags: ['4'],
+    nodes: [
+      {
+        text: 'Child 1',
+        href: '#child1',
+        tags: ['2'],
+        nodes: [
+          {
+            text: 'Grandchild 1',
+            href: '#grandchild1',
+            tags: ['0']
+          },
+          {
+            text: 'Grandchild 2',
+            href: '#grandchild2',
+            tags: ['0']
+          }
+        ]
+      },
+      {
+        text: 'Child 2',
+        href: '#child2',
+        tags: ['0']
       }
- }
+    ]
+  },
+  {
+    text: 'Parent 2',
+    href: '#parent2',
+    tags: ['0']
+  },
+  {
+    text: 'Parent 3',
+    href: '#parent3',
+     tags: ['0']
+  },
+  {
+    text: 'Parent 4',
+    href: '#parent4',
+    tags: ['0']
+  },
+  {
+    text: 'Parent 5',
+    href: '#parent5'  ,
+    tags: ['0']
+  }
+];
 
- function erreurTableur(response){
-    showErrorMessage('Error: ' + response.result.error.message);
- }*/
+$('#treeview_videos').treeview({
+  color: "#428bca",
+  showBorder: false,
+  data: defaultData
+});
