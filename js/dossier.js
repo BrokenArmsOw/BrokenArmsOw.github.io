81d4c38815599a9bff535518a2aac6aaaf5700a3
Adrien BONFILS <Fieldrain@users.noreply.github.com>
2018-01-27 15:12:13 +0100

    change class  to prototype

--- js/dossier.js
+++ js/dossier.js
@@ -1,47 +1,59 @@
-class Dossier{
-    constructor(){
-        this.id = '';
-        this.fichiers = [];
-        
-    }
+var Dossier = function(){
+    var _ = {
+        id = '',
+        fichiers = []
+    };   
+    
+    this.get = function(variable) 
+    { 
+        return _[variable];
+    };
+    
+    this.set = function(variable, val) 
+    { 
+        if(_[variable] !== undefined)
+            _[variable] = val; 
+    };
+};
 
-    get(){
-        let requete = gapi.client.drive.files.list({
-            q : "name = 'Replay' and mimeType = 'application/vnd.google-apps.folder' and sharedWithMe = true"
-        });
+Dossier.prototype.getDossier = function()
+{
+    let requete = gapi.client.drive.files.list({
+        q : "name = 'Replay' and mimeType = 'application/vnd.google-apps.folder' and sharedWithMe = true"
+    });
 
-        requete.execute(this.reponseDossier);
-    }
+    requete.execute(this.reponseDossier);
+};
 
-    reponseDossier(reponse){
-        if (!reponse.error) {
-            let folder = reponse.files[0];
-            console.log(this);
-            this.id = folder.id;
-            this.lireFichiers();
-        }else{
-            showErrorMessage("Erreur: " + reponse.error.message);
-        }
+Dossier.prototype.reponseDossier = function(reponse){
+    if (!reponse.error) {
+        let folder = reponse.files[0];
+        this.set('id',folder.id);
+        this.lireFichiers();
+    }else{
+        showErrorMessage("Erreur: " + reponse.error.message);
     }
+};
 
-    lireFichiers(){
-        let requete = gapi.client.drive.files.list({
-            q : "mimeType = 'application/vnd.google-apps.spreadsheet' and '"+ this.id +"' in parents"
-        });
-        
-        requete.execute(this.reponseFichier);
-    }
+Dossier.prototype.lireFichiers = function(){
+    let requete = gapi.client.drive.files.list({
+        q : "mimeType = 'application/vnd.google-apps.spreadsheet' and '"+ this.get('id') +"' in parents"
+    });
+    
+    requete.execute(this.reponseFichier);
+};
 
-    reponseFichier(reponse){
-        if (!reponse.error) {
-            for(i=0;i<reponse.files.length;i++){
-                let f = reponse.files[i];
-                let fichier = new Fichier(fichier.id,fichier.name);
-                fichier.get();
-                fichiers.push(fichier);
-            } 
-        }else{
-            showErrorMessage("Erreur: " + reponse.error.message);
-        }
+Dossier.prototype.reponseDossier = function(reponse){
+    if (!reponse.error) {
+        for(i=0;i<reponse.files.length;i++){
+            let f = reponse.files[i];
+            let fichier = new Fichier(fichier.id,fichier.name);
+            fichier.get();
+
+            let fs = this.get('fichiers');
+            fs.push(fichier);
+        } 
+    }else{
+        showErrorMessage("Erreur: " + reponse.error.message);
     }
-}
+};
--- js/fichier.js
+++ js/fichier.js
@@ -1,31 +1,42 @@
-class Fichier{
-    constructor(Id,Name){
-        this.id = Id;
-        this.name = Name;
-        this.data;
+var Fichier = function(Id,Name){
+    var _ = {
+        id = Id,
+        name = name,
+        data = null
+    };   
+    
+    this.get = function(variable) 
+    { 
+        return _[variable];
+    };
+    
+    this.set = function(variable, val) 
+    { 
+        if(_[variable] !== undefined)
+            _[variable] = val; 
+    };
+};
+
+Fichier.prototype.getFichier = function(){
+    let requete = gapi.client.sheets.spreadsheets.values.batchGet({
+        spreadsheetId: this.get('id'),
+        ranges: 'Feuille 1!A:E',
+    });
+
+    requete.execute(this.reponseTableur);
+};
+
+Fichier.prototype.reponseTableur = function(reponse){
+    if(!reponse.error){
+        let range = reponse.result;
+        console.log(range);
+        /*if (range.values.length > 0) {
+            
+        } else {
+            
+        }*/
+    }else{
+        showErrorMessage('Error: ' + reponse.result.error.message);
     }
 
-    get(){
-        let requete = gapi.client.sheets.spreadsheets.values.batchGet({
-            spreadsheetId: this.id,
-            ranges: 'Feuille 1!A:E',
-        });
-
-        requete.execute(this.reponseTableur);
-    }
-
-    reponseTableur(reponse){
-        if(!reponse.error){
-            let range = reponse.result;
-            console.log(range);
-            /*if (range.values.length > 0) {
-                
-            } else {
-                
-            }*/
-        }else{
-            showErrorMessage('Error: ' + reponse.result.error.message);
-        }
-        
-    }
-}
+};
